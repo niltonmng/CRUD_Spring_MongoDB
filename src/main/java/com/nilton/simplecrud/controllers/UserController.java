e120a8c806c5d716c0d264259d0561920ef88c96
niltonmng <nilton.ginani@ccc.ufcg.edu.br>
2019-09-23 16:47:41 -0300

    first configurations and PreAuthorize test

--- src/main/java/com/nilton/simplecrud/controllers/UserController.java
+++ src/main/java/com/nilton/simplecrud/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,7 +57,8 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
- 	public ResponseEntity<Void> delete(@PathVariable String id) {
+ 	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
--- src/main/java/com/nilton/simplecrud/security/WebSecurityConfig.java
+++ src/main/java/com/nilton/simplecrud/security/WebSecurityConfig.java
@@ -1,7 +1,34 @@
 package com.nilton.simplecrud.security;
 
-public class WebSecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.httpBasic()
+			.and()
+			.csrf().disable();
+	}
 	
+	@Autowired
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("nilton").password("{noop}daca2019").roles("USER")
+			.and()
+			.withUser("admin").password("{noop}daca2019").roles("ADMIN");
+	}
 
 }
