35975f83809b5da20bf5b7f6330e30c826033eef
niltonmng <nilton.ginani@ccc.ufcg.edu.br>
2019-11-28 23:38:30 -0300

    mods

--- src/main/java/com/nilton/simplecrud/config/Instantiation.java
+++ src/main/java/com/nilton/simplecrud/config/Instantiation.java
@@ -1,64 +1,64 @@
-package com.nilton.simplecrud.config;
-
-
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.TimeZone;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Configuration;
-
-import com.nilton.simplecrud.domain.Post;
-import com.nilton.simplecrud.domain.User;
-import com.nilton.simplecrud.dto.AuthorDTO;
-import com.nilton.simplecrud.dto.CommentDTO;
-import com.nilton.simplecrud.repository.PostRepository;
-import com.nilton.simplecrud.repository.UserRepository;
-
-@Configuration
-public class Instantiation implements CommandLineRunner { // permite que a nossa base de dados inicie com as specs definidas por nós
-
-	
-	@Autowired
-	private UserRepository userRepository;
-	
-	@Autowired
-	private PostRepository postRepository;
-	
-	@Override
-	public void run(String... args) throws Exception {
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
-		sdf.setTimeZone(TimeZone.getTimeZone("GMT")); // consideramos que está no horário de Londres.
-		
-		userRepository.deleteAll();
-		postRepository.deleteAll();
-		
-		User maria = new User(null, "Maria Brown", "maria@gmail.com"); 
-		User alex = new User(null, "Alex Green", "alex@gmail.com"); 
-		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		userRepository.save(maria);
-		userRepository.save(alex);
-		userRepository.save(bob);
-		
-		Post post1 = new Post(null, sdf.parse("23/03/2019"), "Control to Bridge! This is Sally.", "How are you on this lovely morning?", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("23/03/2100"), "Good Morning Sally!", "Another day in Paradise!", new AuthorDTO(maria));
-		
-		CommentDTO c1 = new CommentDTO("Boa viagem amigo!", sdf.parse("23/03/2019"), new AuthorDTO(alex));
-		CommentDTO c2 = new CommentDTO("Volte sempre!", sdf.parse("23/03/2019"), new AuthorDTO(alex));
-		CommentDTO c3 = new CommentDTO("Deus lhe abençõe!", sdf.parse("23/03/2019"), new AuthorDTO(alex));
-		
-		post1.getComment().addAll(Arrays.asList(c1, c2));
-		post2.getComment().addAll(Arrays.asList(c3));
-		
-		postRepository.save(post1);
-		postRepository.save(post2);
-		
-		maria.getPosts().addAll(Arrays.asList(post1, post2));
-		userRepository.save(maria);
-		
-	}
-
-}
+//package com.nilton.simplecrud.config;
+//
+//
+//import java.text.SimpleDateFormat;
+//import java.util.Arrays;
+//import java.util.TimeZone;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Configuration;
+//
+//import com.nilton.simplecrud.domain.Post;
+//import com.nilton.simplecrud.domain.User;
+//import com.nilton.simplecrud.dto.AuthorDTO;
+//import com.nilton.simplecrud.dto.CommentDTO;
+//import com.nilton.simplecrud.repository.PostRepository;
+//import com.nilton.simplecrud.repository.UserRepository;
+//
+//@Configuration
+//public class Instantiation implements CommandLineRunner { // permite que a nossa base de dados inicie com as specs definidas por nós
+//
+//	
+//	@Autowired
+//	private UserRepository userRepository;
+//	
+//	@Autowired
+//	private PostRepository postRepository;
+//	
+//	@Override
+//	public void run(String... args) throws Exception {
+//		
+//		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
+//		sdf.setTimeZone(TimeZone.getTimeZone("GMT")); // consideramos que está no horário de Londres.
+//		
+//		userRepository.deleteAll();
+//		postRepository.deleteAll();
+//		
+//		User maria = new User(null, "Maria Brown", "maria@gmail.com"); 
+//		User alex = new User(null, "Alex Green", "alex@gmail.com"); 
+//		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+//		
+//		userRepository.save(maria);
+//		userRepository.save(alex);
+//		userRepository.save(bob);
+//		
+//		Post post1 = new Post(null, sdf.parse("23/03/2019"), "Control to Bridge! This is Sally.", "How are you on this lovely morning?", new AuthorDTO(maria));
+//		Post post2 = new Post(null, sdf.parse("23/03/2100"), "Good Morning Sally!", "Another day in Paradise!", new AuthorDTO(maria));
+//		
+//		CommentDTO c1 = new CommentDTO("Boa viagem amigo!", sdf.parse("23/03/2019"), new AuthorDTO(alex));
+//		CommentDTO c2 = new CommentDTO("Volte sempre!", sdf.parse("23/03/2019"), new AuthorDTO(alex));
+//		CommentDTO c3 = new CommentDTO("Deus lhe abençõe!", sdf.parse("23/03/2019"), new AuthorDTO(alex));
+//		
+//		post1.getComment().addAll(Arrays.asList(c1, c2));
+//		post2.getComment().addAll(Arrays.asList(c3));
+//		
+//		postRepository.save(post1);
+//		postRepository.save(post2);
+//		
+//		maria.getPosts().addAll(Arrays.asList(post1, post2));
+//		userRepository.save(maria);
+//		
+//	}
+//
+//}
